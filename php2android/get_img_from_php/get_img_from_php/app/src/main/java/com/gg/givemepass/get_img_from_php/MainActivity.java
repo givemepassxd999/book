6fe5cd900a818a63c169d1341738a0d1d51765eb
Rick Wu <rick.wu@mitake.com.tw>
2017-02-08 18:45:07 +0800

    新增上傳圖片到server專案

---
+++ php2android/get_img_from_php/get_img_from_php/app/src/main/java/com/gg/givemepass/get_img_from_php/MainActivity.java
@@ -0,0 +1,24 @@
+package com.gg.givemepass.get_img_from_php;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        initView();
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+
+    }
+}
